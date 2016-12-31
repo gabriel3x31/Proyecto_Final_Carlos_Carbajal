565d41dbd9b0b7db9a00214e82027313f418d959
DESKTOP-CGESD3U\gabo <gabo1693@gmail.com>
2016-12-31 15:56:11 -0600

    Creamos una vizta para mosrar los usuarios registrados en nuestra base de datos creamos el controlador que edita a nuestros usuarios

--- laravel/app/Http/Controllers/ControlaUsuario.php
+++ laravel/app/Http/Controllers/ControlaUsuario.php
@@ -1,89 +1,92 @@
-<?php namespace Cinema\Http\Controllers;
-
+<?php
+namespace Cinema\Http\Controllers;
+use Illuminate\Http\Request;
 use Cinema\Http\Requests;
 use Cinema\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
-
-class ControlaUsuario extends Controller {
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('usuario.create');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store(Request $request)
+use Cinema\User;
+use Session;
+use Redirect;
+class ControlaUsuario extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        \Cinema\User::create([
+        $users = User::all();
+        return view('usuario.index',compact('users'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('usuario.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        User::create([
             'name' => $request['name'],
             'email' => $request['email'],
-            'password' => bcrypt($request['password']),
+            'password' => $request['password'],
         ]);
-        return "Usuario registrado";
+        
+        return redirect('/usuario')->with('message','store');
     }
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('usuario.edit',['user'=>$user]);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->fill($request->all());
+        $user->save();
+        Session::flash('message','Usuario Actualizado Correctamente');
+        return Redirect::to('/usuario');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -4,7 +4,7 @@
 			<div class="top-header">
 				<div class="logo">
 					<a href="index.html"><img src="images/logo.png" alt="" /></a>
-					<p>MoviesCiencias</p>
+					<p>Movie Theater</p>
 				</div>
 				<div class="search">
 					<form>
@@ -16,13 +16,13 @@
 			</div>
 			<div class="header-info">
 				<h1>BIG HERO 6</h1>
-				<p class="age"><a href="#">AAA</a> Don Hall, Chris Williams</p>
+				<p class="age"><a href="#">All Age</a> Don Hall, Chris Williams</p>
 				<p class="review">Rating	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;  8,5/10</p>
-				<p class="review reviewgo">Genero	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : &nbsp;&nbsp; Animacion, Accion, Comedia</p>
-				<p class="review">Lanzamiento &nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp; 7 November 2016</p>
-				<p class="special">El vínculo especial que se desarrolla entre el robot inflable más grande Baymax y el prodigio Hiro Hamada, que se unen con un grupo de amigos para formar una banda de héroes de alta tecnología.</p>
-				<a class="video" href="#"><i class="video1"></i>Trailer</a>
-				<a class="book" href="#"><i class="book1"></i>Boletos</a>
+				<p class="review reviewgo">Genre	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : &nbsp;&nbsp; Animation, Action, Comedy</p>
+				<p class="review">Release &nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp; 7 November 2014</p>
+				<p class="special">The special bond that develops between plus-sized inflatable robot Baymax, and prodigy Hiro Hamada, who team up with a group of friends to form a band of high-tech heroes.</p>
+				<a class="video" href="#"><i class="video1"></i>WATCH TRAILER</a>
+				<a class="book" href="#"><i class="book1"></i>BOOK TICKET</a>
 			</div>
 		</div>
 		<div class="review-slider">
